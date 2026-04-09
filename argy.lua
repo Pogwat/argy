bc773e8e952e8342bafcfa59960de90a5735a376
Pogwat <208688662+Pogwat@users.noreply.github.com>
2026-04-09 13:38:09 -0400

    move tests into seprate file

--- argy.lua
+++ argy.lua
@@ -70,22 +70,4 @@ function argy:gen_fargs()
     end
 end
 
---print(arg[1])
-argy:arg("hi","--hi", "string")
-argy:arg("bi","--bi", "string")
-argy:flag("h","-h", "string")
-argy:positional_arg("am",3, "string")
-argy:positional_arg("mine",4, "string")
-print(argy.args["--hi"])
-
-argy:gen_fargs() 
-print(argy:get("hi"))
--- print(argy.final_args["hi"])
--- print(argy.final_args["am"])
--- print(argy.final_args["mine"])
-
---print(argy.final_args["bi"])
--- print(argy.final_args[2])
---print(argy.final_args)
-
 return argy --this is cool! The parser uses the return
---
+++ test.lua
@@ -0,0 +1,18 @@
+require "argy"
+--print(arg[1])
+argy:arg("hi","--hi", "string")
+argy:arg("bi","--bi", "string")
+argy:flag("h","-h", "string")
+argy:positional_arg("am",3, "string")
+argy:positional_arg("mine",4, "string")
+print(argy.args["--hi"])
+
+argy:gen_fargs() 
+print(argy:get("hi"))
+-- print(argy.final_args["hi"])
+-- print(argy.final_args["am"])
+-- print(argy.final_args["mine"])
+
+--print(argy.final_args["bi"])
+-- print(argy.final_args[2])
+--print(argy.final_args)
