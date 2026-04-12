b9317b4f5ed81e456e5a4d43615072f4ef42d7e6
Pogwat <208688662+Pogwat@users.noreply.github.com>
2026-04-12 01:38:46 -0400

    adapt funcitons for new argy table structure

--- argy.lua
+++ argy.lua
@@ -2,41 +2,52 @@
 local ahandler = function(table,key) return table[key] .." is not a value in " .. table end
 argy = {
     positional_args = {
-        __arg_type = "positional_argument", 
-        __index = ahandler,
-        __index_type = "number",
-        __len = 4
+        args = {},
+        arg_type = "positional_argument",
+        name_type = "number",
+        len = 0,
+        __index = function(table,key) return table.args[key] end
     }, -- arg,
 
     args = {
-        __arg_type = "argument",
-        __index = ahandler,
-        __index_type = "string",
-        __len = 4
+        args = {},
+        arg_type = "argument",
+        name_type = "string",
+        len = 0,
+        __index = function(table,key) return table.args[key] end
     }, -- -a --arg
 
     flags = {
-        __arg_type = "flag",
-        __index = ahandler,
-        __index_type = "string",
-        __len = 4
+        args = {},
+        arg_type = "flag",
+        name_type = "string",
+        len = 0,
+        __index = function(table,key) return table.args[key] end
     }, -- -f
 
-    unused_args = {},
+    unused_args = {
+        args = {},
+        len = 0,
+        __index = function(table,key) return table.args[key] end
+    },
 
-    final_args = {}
+    final_args = {
+        args = {},
+        len = 0,
+        __index = function(table,key) return table.args[key] end
+    }
 }
 
 function argy:initalizers(arg_table, assert_callback)
     assert_callback = assert_callback or function() end
     return function(self,name,arg_ident, input_type, description) -- fix for the ":" funciton calls which pass self as first arg
-    local arg_type = arg_table.__arg_type
-    local arg_index_type = arg_table.__index_type
-    assert(type(arg_ident) == arg_index_type, arg_type.." "..arg_ident.." is not of type "..arg_index_type )
+    local arg_type = arg_table.arg_type
+    local arg_name_type = arg_table.name_type
+    assert(type(arg_ident) == arg_name_type, arg_type.." "..arg_ident.." is not of type "..arg_name_type )
     assert_callback(arg_ident,arg_type)
-    arg_table[arg_ident] = name
-    arg_table["__len"] = arg_table["__len"]+1
-    self.final_args[name] = {type = input_type, arg_table = arg_table, description = description}
+    arg_table.args[arg_ident] = name
+    arg_table["len"] = arg_table["len"]+1
+    self.final_args.args[name] = {type = input_type, arg_table = arg_table, description = description}
     end
 end
 
@@ -57,26 +68,28 @@ argy.positional_arg = argy:initalizers(argy.positional_args)
 argy.arg = argy:initalizers(argy.args, argy.assert_arg)
 argy.flag = argy:initalizers(argy.flags, argy.assert_flag)
 
-function argy:get(name) return self.final_args[name].value end
+function argy:get(name) 
+    return self.final_args.args[name].value 
+end
 
 function argy:get_unused(position) return self.unused_args[position] end
 
 function argy:is_string_arg_or_flag(arg_string)
-    if self.args[arg_string]~=nil then return self.args.__arg_type end
-    if self.flags[arg_string]~=nil then return self.flags.__arg_type end
+    if self.args.args[arg_string]~=nil then return self.args.arg_type end
+    if self.flags.args[arg_string]~=nil then return self.flags.arg_type end
 end
 
-function argy:name_arg_type(name) return self.final_args[name].arg_table.__arg_type end
+function argy:name_arg_type(name) return self.final_args[name].arg_table.arg_type end
 
 function argy:is_index_pos_arg(index) 
-    if self.positional_args[index]~=nil then return  self.positional_args.__arg_type end
+    if self.positional_args.args[index]~=nil then return  self.positional_args.arg_type end
 end
 
 function argy:handle_arg_type(arg_type, position)
     local types = {
-        [self.args.__arg_type] = {value = arg[position+1]  , skip =2, table = self.args, table_index = arg[position]},
-        [self.flags.__arg_type] =  {value = true, skip = 1, table = self.flags, table_index = arg[position]},
-        [self.positional_args.__arg_type] =  {value = arg[position], skip = 1, table = self.positional_args, table_index = position }
+        [self.args.arg_type] = {value = arg[position+1]  , skip =2, table = self.args.args, table_index = arg[position]},
+        [self.flags.arg_type] =  {value = true, skip = 1, table = self.flags.args, table_index = arg[position]},
+        [self.positional_args.arg_type] =  {value = arg[position], skip = 1, table = self.positional_args.args, table_index = position }
     }
     return types[arg_type]
 end
@@ -89,12 +102,13 @@ function argy:gen_fargs()
         if arg_type~=nil then
             local handler = self:handle_arg_type(arg_type, position)
             local arg_name = handler.table[handler.table_index] or error(arg_string .. " did not match a table")
-            self.final_args[arg_name].value = handler.value 
+            self.final_args.args[arg_name].value = handler.value 
             position=position+handler.skip
         else
             self.unused_args[position] = arg_string
             position=position+1
         end
+
     end
 end
 
--- help.lua
+++ help.lua
@@ -9,9 +9,9 @@ local earg_len = exclude_args["__len"]
 
 function argy:gen_desc_lines(arg_table)
     local args_buf = ""
-    for arg_string,arg_name in pairs(arg_table) do --Order is not deterministic so args are randomly order in help
+    for arg_string,arg_name in pairs(arg_table.args) do --Order is not deterministic so args are randomly order in help
         if exclude_args[arg_string] == nil then
-            local description = self.final_args[arg_name].description or ""
+            local description = self.final_args.args[arg_name].description or ""
             args_buf = args_buf..arg_string.."\n  "..description.."\n\n"
         end
     end
@@ -19,8 +19,8 @@ function argy:gen_desc_lines(arg_table)
 end
 
 function argy:gen_help()
-    local args_len = self.args["__len"]
-    local flags_len = self.flags["__len"] 
+    local args_len = self.args.len
+    local flags_len = self.flags.len
 
     local flags_buf =  "FLAGS:\n" .. argy:gen_desc_lines(self.flags) or ""
     local args_buf =  "ARGS:\n" .. argy:gen_desc_lines(self.args)  or ""
