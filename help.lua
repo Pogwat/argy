fb5b37f4ba5ade60c4e5533df31986bf2fe21965
Pogwat <208688662+Pogwat@users.noreply.github.com>
2026-04-14 20:01:23 -0400

    add proper exclude args checks

--- help.lua
+++ help.lua
@@ -4,13 +4,16 @@ argy.inputs.flags:flag("help", "-h", "boolean","help")
 
 local program_name = arg[0]
 local help = program_name
-local exclude_args={__arg_type=true, __index=true ,__index_type=true, __len=4}
-local earg_len = exclude_args["__len"]
+
+local exclude_args= {
+    args = {__arg_type=true, __index=true ,__index_type=true},
+    len=4
+}
 
 function argy:gen_desc_lines(arg_table)
     local args_buf = ""
     for arg_string,arg_name in pairs(arg_table.args) do --Order is not deterministic so args are randomly order in help
-        if exclude_args[arg_string] == nil then
+        if exclude_args.args[arg_string] == nil then
             local description = self.outputs.final_args.args[arg_name].description or ""
             args_buf = args_buf..arg_string.."\n  "..description.."\n\n"
         end
@@ -19,14 +22,13 @@ function argy:gen_desc_lines(arg_table)
 end
 
 function argy:gen_help()
-    local args_len = self.inputs.args.len
-    local flags_len = self.inputs.flags.len
-
+    arguments_v_exclude_contains, arguments_v_exclude_not_contains = check_table_for_a_tables_keys(self.inputs.args.args, exclude_args)
+    flags_v_exclude_contains, flags_v_exclude_not_contains = check_table_for_a_tables_keys(self.inputs.flags.args, exclude_args)
+    if arguments_v_exclude_contains.len~=0 then help = help .. " [ARGUMENTS]" end
+    if flags_v_exclude_contains.len~=0 then help = help .. " [FLAGS]" end
+    
     local flags_buf =  "FLAGS:\n" .. argy:gen_desc_lines(self.inputs.flags) or ""
     local args_buf =  "ARGS:\n" .. argy:gen_desc_lines(self.inputs.args)  or ""
-
-    if args_len>earg_len then help = help .. " [ARGUMENTS]" end
-    if flags_len>earg_len then help = help .. " [FLAGS]" end
     help = help.."\n".. flags_buf .. args_buf
 
     if argy.outputs.final_args:get("help").value~=nil then
@@ -35,6 +37,26 @@ function argy:gen_help()
     end
 
 end
+
+function check_table_for_a_tables_keys(table_to_check, refrence_table)
+    local contains_table = {kv_pairs = {},len = 0}
+    local dosent_contains_table = {kv_pairs = {},len = 0}
+    for k,v in pairs(refrence_table) do
+        
+        if table_to_check[k] then 
+            contains_table.kv_pairs[k] = table_to_check[k] 
+            contains_table.len = contains_table.len+1
+        else
+            dosent_contains_table.kv_pairs[k] = table_to_check[k] 
+            dosent_contains_table.len = dosent_contains_table.len+1
+        end
+
+    end
+    return contains_table, dosent_contains_table
+end
+
+
+
 
 -- cmd [OPTIONS] [FLAGS] [POSITIONAL]
 
